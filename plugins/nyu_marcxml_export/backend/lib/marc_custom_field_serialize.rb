7cb2d7deee0913726678e20e0c891c679534a72e
Esha Datta <eshadatta16@gmail.com>
2016-03-06 23:27:30 -0500

    added methods to DRY out code

--- plugins/nyu_marcxml_export/backend/lib/marc_custom_field_serialize.rb
+++ plugins/nyu_marcxml_export/backend/lib/marc_custom_field_serialize.rb
@@ -2,7 +2,7 @@ class MARCCustomFieldSerialize
   ControlField = Struct.new(:tag, :text)
   DataField = Struct.new(:tag, :ind1, :ind2, :subfields)
   SubField = Struct.new(:code, :text)
-  
+  Extra_fields = []
   def initialize(record)
     @record = record
  
@@ -18,13 +18,30 @@ class MARCCustomFieldSerialize
 
   def datafields
     extra_fields = []
-    extra_fields << DataField.new('853', '0', '0', [SubField.new('8','1'),SubField.new('a','Box')])
-    tc = @record.aspace_record['top_containers']
-    (@record.datafields + extra_fields).sort_by(&:tag)
+    add_853_tag
+    #extra_fields << DataField.new('853', '0', '0', [SubField.new('8','1'),SubField.new('a','Box')])
+    top_containers = @record.aspace_record['top_containers']
+    top_containers.each_pair { |barcode, indicator|
+      add_863_tag(barcode,indicator)
+    }
+    (@record.datafields + Extra_fields).sort_by(&:tag)
 
   end
 
-  #def add_853_tag
-   # DataField.new('853', '0', '0', [SubField.new('8','1'),SubField.new('a','Box')])
-  #end
+
+  def add_853_tag
+    Extra_fields << DataField.new('853', '0', '0', 
+      [SubField.new('8','1'),SubField.new('a','Box')])
+  end
+
+  def add_863_tag(barcode, indicator)
+    Extra_fields << DataField.new('863', '', '', 
+      [SubField.new('8',"1.#{indicator}"),
+        SubField.new('a',indicator),
+        SubField.new('p',barcode)])
+  end
+
+  def add_949_tag(extra_fields, barcode, indicator)
+    extra_fields << DataField.new('863', '', '', [SubField.new('8',"1.#{indicator}"),SubField.new('a',indicator),SubField.new('p',barcode)])
+  end
 end
