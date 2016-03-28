3b35ea416c8efab6a9cfe99e3ee9a4e99cdbaaca
Esha Datta <eshadatta16@gmail.com>
2016-03-28 10:50:36 -0400

    added test to check for indicator attribute values

--- plugins/nyu_marcxml_export/backend/spec/export_custom_marc_spec.rb
+++ plugins/nyu_marcxml_export/backend/spec/export_custom_marc_spec.rb
@@ -10,17 +10,23 @@ describe 'NYU Custom Marcxml Export' do
 	    @marc = get_marc(@resource)
 	  end
 
+	 it 'should have the correct indicator attribute values' do
+	 	@marc.should have_tag("datafield[@tag='853'][@ind1='0'][@ind2='0']")
+    end
 
-	  it "maps the value '1' to subfield '8'" do
+	 it "maps the value '1' to subfield '8'" do
       @marc.should have_tag "datafield[@tag='853']/subfield[@code='8']" => '1'
     end
 
     it "maps the value 'Box' to subfield 'a'" do
       @marc.should have_tag "datafield[@tag='853']/subfield[@code='a']" => 'Box'
     end
+
    end
 
    describe 'datafield 863 mapping' do
+     # default create behavior is to 
+     # create arbitrary indicator and barcode values
      let (:top_container) { create(:json_top_container) }
 	  before(:each) do
 	  	 resource = create_resource
@@ -30,6 +36,11 @@ describe 'NYU Custom Marcxml Export' do
 	  	  	                       "instances" => [build_instance(top_container)])
 	    @marc = get_marc(resource)
 	  end
+
+	  it 'should have the correct indicator attribute values' do
+	 	@marc.should have_tag("datafield[@tag='863'][@ind1=''][@ind2='']")
+     end
+
 	  # these marc fields should exist whether or not barcode is there
      it "concatenates '1.' with the indicator value to subfield '8'" do
         @marc.should have_tag "datafield[@tag='863']/subfield[@code='8']" => "1.#{top_container.indicator}"
@@ -39,7 +50,7 @@ describe 'NYU Custom Marcxml Export' do
         @marc.should have_tag "datafield[@tag='863']/subfield[@code='a']" => "#{top_container.indicator}"
      end
 
-     it "map the barcode value to subfield 'p' if barcode exists" do
+     it "maps the barcode value to subfield 'p' if barcode exists" do
        @marc.should have_tag "datafield[@tag='863']/subfield[@code='p']" => "#{top_container.barcode}"
      end
   	  context 'there is no barcode in the top container' do
@@ -58,4 +69,8 @@ describe 'NYU Custom Marcxml Export' do
        end
      end
    end
+
+   #describe 'datafield 949 mapping' do
+
+   #end
 end
